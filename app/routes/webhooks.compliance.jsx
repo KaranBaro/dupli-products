3c55ea8b5bb8400997a16af753d6b11b029d80bb
KaranBaro <karanbaro.kb@gmail.com>
2026-04-30 12:15:21 +0530

    Added new files

---
+++ app/routes/webhooks.compliance.jsx
@@ -0,0 +1,7 @@
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }) => {
+  await authenticate.webhook(request);
+
+  return new Response("OK", { status: 200 });
+};
